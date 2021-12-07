59220a5a13d6a16267e9ed34c4a1c19c7c9fd425
surajdarekar95 <darekarsuraj10395@gmail.com>
2021-12-08 01:20:35 +0530

    RetryAnalyzer

---
+++ TestNGLogics/src/test/java/TestCases/MyTest1.java
@@ -0,0 +1,27 @@
+package TestCases;
+
+import org.testng.Assert;
+
+import org.testng.annotations.Test;
+
+public class MyTest1 {
+	
+	@Test
+	public void Test1() {
+
+		Assert.assertEquals(false, true);
+	}
+
+	@Test
+	public void Test2() {
+
+		Assert.assertEquals(false, true);
+
+	}
+	
+	@Test
+	public void Test3() {
+		Assert.assertEquals(true, true);;
+	}
+
+}
